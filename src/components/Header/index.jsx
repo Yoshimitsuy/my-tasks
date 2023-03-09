4edc75e0733f596d571afcf03a8dcf785ac86a28
yoshimitsuy <rafael.lacana7@gmail.com>
2023-03-09 05:21:54 -0300

    css: button and input

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import styles from './header.module.css';
+import todoLogo from '../../assets/todoLogo.svg';
 
 export function Header() {
   return (
     <header className={ styles.header } >
-      <p>header</p>
+      <img src={todoLogo} alt="" />
+
+      <form className={ styles.newTaskForm }>
+        <input type="text" placeholder='add a new task' />
+        <button>create</button>
+      </form>
     </header>
-  )
+  );
 }
