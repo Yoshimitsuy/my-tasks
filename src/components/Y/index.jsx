aee647e0d95708460b0396a20bd2f53b1eb475e2
yoshimitsuy <rafael.lacana7@gmail.com>
2023-03-18 04:35:53 -0300

    add a y comp

---
+++ src/components/Y/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './y.module.css';
+
+export function Y() {
+  return (
+    <div className={ styles.author }>
+      <a href='https://yoshimitsuy-io.vercel.app/' target='_blank' rel='noreferrer'>
+        <button >Rafael Yoshimitsuy</button>
+      </a>  
+    </div>
+  );
+}
